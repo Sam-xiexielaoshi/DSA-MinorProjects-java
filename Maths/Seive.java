3d56759a2b57e86939678a0df7748b2dfa17270f
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-09-05 21:04:47 +0530

    Add Seive class for prime number generation and XORrangw class for range XOR calculation; implement flipImage class for flipping and inverting a 2D binary image

---
+++ Maths/Seive.java
@@ -0,0 +1,22 @@
+package Maths;
+
+public class Seive {
+    public static void main(String[] args) {
+
+    }
+
+    static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
---
+++ leetcode/flipImage.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class flipImage {
+    public static void main(String[] args) {
+        
+    }
+
+    public int[][] flipAndInvertImage(int[][]image){
+        for(int[] row:image){
+            for(int i=0;i<(image[0].length+1)/2;i++){
+                int temp = row[i]^1;
+                row[i] = row[image[0].length-1-i]^1;
+                row[image[0].length-i-1]=temp;
+            }
+        }return image;
+    }
+}
