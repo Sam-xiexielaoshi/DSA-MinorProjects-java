3d79ec04c214f74a161872acce2f63b3afeb5780
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-09-07 21:53:27 +0530

    Fix formatting in arraylist class; add space between ArrayList type and variable name

--- Collectionframeworks/arraylist.java
+++ Collectionframeworks/arraylist.java
@@ -1,15 +1,15 @@
 package Collectionframeworks;
 
 import java.util.ArrayList;
+
 public class arraylist {
 
-
     public static void main(String[] args) {
-        ArrayList<Integer>list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(4);
         list.add(6);
         System.out.println(list.get(2));
-    
-}
+
+    }
 }
